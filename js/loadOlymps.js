60b14a579ffdb59675115cde359c337cbe904b6a
Sergei Tundykov <sstund@bk.ru>
2024-06-07 21:28:16 +0300

    first commit

--- js/loadOlymps.js
+++ js/loadOlymps.js
@@ -13,7 +13,7 @@ function loadFile(filePath) {
 const olympLists = ["rsosh-20_21.csv", "rsosh-21_22.csv", "rsosh-22_23.csv", "rsosh-23_24.csv", "vsosh.csv"];
 
 for(let i = 0; i < olympLists.length; ++i) {
-  let text = loadFile("./res/olymps/"+olympLists[i]);
+  let text = loadFile("./res/olymps/"+olympLists[i]).split("\n");
   console.log(text);
 }
 
