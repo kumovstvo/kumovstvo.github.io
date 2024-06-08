66d9fa6069404c389a98a10048acbd040e926b1a
Sergei Tundykov <sstund@bk.ru>
2024-06-08 13:01:25 +0300

    123

--- js/loadOlymps.js
+++ js/loadOlymps.js
@@ -67,36 +67,6 @@ document.querySelector('#selectOlymp').addEventListener('change', function (e) {
   olympNow = olympName;
 });
 
-
-
-
-// const olympLists = ["rsosh-20_21.csv", "rsosh-21_22.csv", "rsosh-22_23.csv", "rsosh-23_24.csv", "vsosh.csv"];
-
-// for(let i = 0; i < olympLists.length; ++i) {
-//   let text = loadFile("./res/olymps/"+olympLists[i]).split("\n");
-//   Set
-
-  // let datalistHtml = `
-  // <select name="olymp">
-  //   <option disabled selected value="s1">Выберите олимпиаду</option>
-  // `;
-  // for(let j = 0; j < text.length; ++j) {
-  //   text[j] = text[j].split(";");
-  //   datalistHtml += `
-  //   <option value="olymp${j}">${text[j][0]}</option>
-  //   `
-  // }
-
-  
-//   //   
-//   //   <option value="vsosh.csv">ВсОШ</option>
-//   //   <option value="rsosh-20_21.csv">РСОШ 2020-21</option>
-//   //   <option value="rsosh-21_22.csv">РСОШ 2021-22</option>
-//   //   <option value="rsosh-22_23.csv">РСОШ 2022-23</option>
-//   //   <option value="rsosh-23_24.csv">РСОШ 2023-24</option>
-//   // </select>
-//   `
-//   console.log(text);
-// }
-
-
+function addAch() {
+  alert(123);
+}
