130aa3e4364ab54187e3e81e3a577e47e2ae0ec1
Sergei Tundykov <sstund@bk.ru>
2024-06-09 12:12:24 +0300

    123

--- js/checkLgots.js
+++ js/checkLgots.js
@@ -199,7 +199,7 @@ function updateLgots() {
                     if(j != '+') lgots[j].push([Number(U.lgota), ol])
                   }
                 } else if(lgots.hasOwnProperty(U.predmet)) lgots[U.predmet].push([Number(U.lgota), ol]);
-                else lgots["+"].push([Number(U.lgota), ol+" (ОШИБКА: Нет предмета для подтверждения БВИ)"]);
+                else lgots["+"].push([Number(U.lgota), ol+" (ОШИБКА: Нет предмета для подтверждения)"]);
               }
             }
           }
