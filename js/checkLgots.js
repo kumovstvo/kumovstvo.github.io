fbf258280e36ce26a2a1b5aae1f5c83862b1f7b1
Sergei Tundykov <sstund@bk.ru>
2024-06-20 17:55:01 +0300

    1

--- js/checkLgots.js
+++ js/checkLgots.js
@@ -55,7 +55,7 @@ for(const vuz of vuzs) {
         Number(OLYMPS[list][olymp][profile]) <= minLevel) {
           for(const pr of prgrs) {
             if(pr[0] == '#') {
-              for(let _pg of programs.keys) {
+              for(let _pg of programs) {
                 if(String(_pg.ege).includes(pr[1])) {
                   programs[pr].lgots[list][olymp+` (${profile})`].push({
                     status: minStatus,
